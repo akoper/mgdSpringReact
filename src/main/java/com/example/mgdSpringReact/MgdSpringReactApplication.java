4ef6b27ab44adf478551f5cf94d1e62a126fae66
andrew_koper <andrew.koper@gmail.com>
2025-08-30 00:46:08 -0400

    Initial commit of Spring Boot framework and packages/dependencies

---
+++ src/main/java/com/example/mgdSpringReact/MgdSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.mgdSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgdSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgdSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mgdSpringReact/MgdSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mgdSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgdSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
